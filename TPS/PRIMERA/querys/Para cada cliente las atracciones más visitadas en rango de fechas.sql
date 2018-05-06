3b7a57a12ba29b894fc718cc1c4623a086517a1a
Guido Tripodi <guido.tripodi@hotmail.com>
2018-05-05 22:08:51 -0300

    arriba con store me queda arreglar uno de clientes

--- TPS/PRIMERA/querys/Para cada cliente las atracciones más visitadas en rango de fechas.sql
+++ TPS/PRIMERA/querys/Para cada cliente las atracciones más visitadas en rango de fechas.sql
@@ -1,8 +1,12 @@
-SELECT cliente.nombre, cliente.apellido, producto.nombre, count(*) as cantidadVisitado FROM mydb.Cliente cliente, mydb.Tarjeta tarjeta, mydb.Producto producto, mydb.Consumo consumo where
-	consumo.idTarjeta = tarjeta.idTarjeta and tarjeta.idCliente = cliente.idCliente and producto.idProducto = consumo.idProducto and
-    (SELECT COUNT(*) FROM mydb.Consumo consumo where tarjeta.idTarjeta = consumo.idTarjeta
-    and producto.idProducto = consumo.idProducto) >= (SELECT COUNT(*) FROM mydb.Consumo consumo, mydb.Producto producto2 where tarjeta.idTarjeta = consumo.idTarjeta
-    and producto2.idProducto = consumo.idProducto
-    and producto2.idProducto != producto.idProducto)
+SELECT cliente.nombre, cliente.apellido, producto.nombre, count(*) as cantidadVisitado FROM 
+mydb.Cliente cliente, mydb.Tarjeta tarjeta, mydb.Producto producto, mydb.Consumo consumo where
+	consumo.idTarjeta = tarjeta.idTarjeta and 
+    tarjeta.idCliente = cliente.idCliente and
+    producto.idProducto = consumo.idProducto 
 group by consumo.idTarjeta, consumo.idProducto
+Having count(*) >= ALL (Select count(*) as count from mydb.Cliente cliente, mydb.Tarjeta tarjeta, mydb.Producto producto, mydb.Consumo consumo where
+	consumo.idTarjeta = tarjeta.idTarjeta and 
+    tarjeta.idCliente = cliente.idCliente and
+    producto.idProducto = consumo.idProducto 
+    group by consumo.idTarjeta, consumo.idProducto)
 ;
